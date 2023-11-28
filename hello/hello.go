8b4ed340863e594766b1896e68c0c9f9a9626991
marlon savian <marlon.savian@gmail.com>
2023-11-28 19:15:35 -0300

    refactor(hello): refactor hello test and function

--- hello/hello.go
+++ hello/hello.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 
-func Hello(name string) string {
-	return "Hello, " + name
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string ) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
