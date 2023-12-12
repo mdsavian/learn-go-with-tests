b8db1153f8bc178398e30743a3815604457c6c5e
mdsavian <marlon.savian@gmail.com>
2023-12-11 21:06:38 -0300

    refactor(arrays): refactor assertion on test

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -24,9 +24,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	assertCorrectSum(t, got, want)
 }
 
 func TestSumAllTails(t *testing.T) {
@@ -35,18 +33,21 @@ func TestSumAllTails(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertCorrectSum(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertCorrectSum(t, got, want)
 	})
 
+}
+
+func assertCorrectSum(t testing.TB, got, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
