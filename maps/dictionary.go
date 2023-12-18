85da658f0a17f9069adb6783c3f222127e94a9b1
mdsavian <marlon.savian@gmail.com>
2023-12-18 08:39:44 -0300

    refactor(maps): create DictionaryError

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,13 +1,17 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
 
-var (
-	ErrorNotFound   = errors.New("could not find the word you were looking for")
-	ErrorWordExists = errors.New("cannot add word because it already exists")
+const (
+	ErrorNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrorWordExists = DictionaryErr("cannot add word because it already exists")
 )
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(key string) (string, error) {
 
