c38389e3a091e12ef409723e5084ff410f0f2b6f
mdsavian <marlon.savian@gmail.com>
2023-12-19 20:07:59 -0300

    feat(maps): check if word exists before updating it

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -3,8 +3,9 @@ package main
 type Dictionary map[string]string
 
 const (
-	ErrorNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrorWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrorNotFound       = DictionaryErr("could not find the word you were looking for")
+	ErrorWordExists     = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exists")
 )
 
 type DictionaryErr string
@@ -40,8 +41,18 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
-func (d Dictionary) Update(word, definition string) {
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
 
-	d[word] = definition
+	switch err {
+	case ErrorNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
 
 }
