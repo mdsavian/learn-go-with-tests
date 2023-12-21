b7e1d7db91618a71b4bec22d4256d3392b038951
mdsavian <marlon.savian@gmail.com>
2023-12-21 09:29:26 -0300

    refactor(mocking): create const to countdown start and final word

--- mocking/mock.go
+++ mocking/mock.go
@@ -6,13 +6,16 @@ import (
 	"os"
 )
 
+const countdownStart = 3
+const finalWord = "Go!"
+
 func Countdown(writer io.Writer) {
 
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(writer, i)
 	}
 
-	fmt.Fprint(writer, "Go!")
+	fmt.Fprint(writer, finalWord)
 }
 
 func main() {
