a20abfe8b743c1c5f8b7bee774e6f5212a28aa21
mdsavian <marlon.savian@gmail.com>
2024-02-20 09:57:31 -0300

    feat(select): add configurable racer to test timeout error

--- select/racer.go
+++ select/racer.go
@@ -6,17 +6,24 @@ import (
 	"time"
 )
 
+var tenSecondTime = 10 * time.Second
+
 func Racer(a, b string) (winner string, err error) {
+
+	return ConfigurableRacer(a, b, tenSecondTime)
+
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
 
 	select {
 	case <-ping(a):
 		return a, nil
 	case <-ping(b):
 		return b, nil
-	case <-time.After(10 * time.Second):
+	case <-time.After(timeout):
 		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
-
 }
 
 func ping(url string) chan struct{} {
--- select/racer_test.go
+++ select/racer_test.go
@@ -20,7 +20,11 @@ func TestRacer(t *testing.T) {
 		fastURL := fastServer.URL
 
 		want := fastURL
-		got, _ := Racer(slowURL, fastURL)
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
@@ -28,13 +32,11 @@ func TestRacer(t *testing.T) {
 	})
 
 	t.Run("returns an error if a server doesn't respond withing 10s", func(t *testing.T) {
-		slowServer := makeDelayedServer(11 * time.Second)
-		fastServer := makeDelayedServer(12 * time.Second)
+		server := makeDelayedServer(25 * time.Millisecond)
 
-		defer slowServer.Close()
-		defer fastServer.Close()
+		defer server.Close()
 
-		_, err := Racer(slowServer.URL, fastServer.URL)
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
 		if err == nil {
 			t.Error("expected an error but didn't get one")
