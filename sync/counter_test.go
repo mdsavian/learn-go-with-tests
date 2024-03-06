33e337b3bd9537a35522b3ce3e8c4e270ce11ad3
mdsavian <marlon.savian@gmail.com>
2024-03-06 19:09:22 -0300

    feat(sync): lock the counter with sync.Mutex

--- sync/counter_test.go
+++ sync/counter_test.go
@@ -1,16 +1,27 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing counter three times leaves it at 3", func(t *testing.T) {
 
 		counter := Counter{}
-		counter.Inc()
-		counter.Inc()
-		counter.Inc()
+		wantedCount := 1000
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
 
-		assertCount(t, counter, 3)
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCount(t, counter, wantedCount)
 	})
 }
 
